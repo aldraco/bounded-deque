675a1698c18c5d74b01506a8820d4691f73d8db1
aldraco <a.l.drake713@gmail.com>
2016-09-22 20:48:48 -0400

    Initial Commit

---
+++ deque/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  deque
+//
+//  Created by Ashley Drake on 9/22/16.
+//  Copyright © 2016 Ashley Drake. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
